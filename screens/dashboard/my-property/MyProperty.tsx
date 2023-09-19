c8aa88cc9614568ad68f1f859df93479e0bcb097
unknown <lisandro.batiancila@gmail.com>
2023-09-20 05:32:49 +0800

    [feature][san] vehicle property

--- screens/dashboard/my-property/MyProperty.tsx
+++ screens/dashboard/my-property/MyProperty.tsx
@@ -1,13 +1,32 @@
-import { View, Text, Button, TouchableOpacity, ScrollView, FlatList, Image, Dimensions, Modal, StyleSheet } from 'react-native';
+import { View, Text, Image, Button, TouchableOpacity, ScrollView, FlatList, Dimensions, Modal, StyleSheet } from 'react-native';
 import { useUserContext } from '../../../context/User/UserContext';
 import DocumentPicker from 'react-native-document-picker';
 import { useState } from 'react';
 import AssmrModal from '../../../components/modal/Modal';
 import { Card, TextInput } from 'react-native-paper';
+import VehicleProperty from '../../../components/upload-property/VehicleProperty';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 const MyPropertiesScreen = () => {
     const userContext = useUserContext();
     const [openModal, setOpenModal] = useState<boolean>(false);
+
+    const [onOpenPropToUpload, setOnOpenPropToUpload] = useState<boolean>(false);
+    const [propToUploadValue, setPropToUploadValue] = useState(null);
+    const [propToUploadItems, setPropToUploadItems] = useState<{label: string, value: string}[]>([
+        {
+            label: "Vehicle",
+            value: "Vehicle"
+        },
+        {
+            label: "Jewelry",
+            value: "Jewelry"
+        },
+        {
+            label: "Realestate",
+            value: "Realestate"
+        }
+    ]);
 
     const onOpenGallery = async () => {
         try{
@@ -23,53 +42,65 @@ const MyPropertiesScreen = () => {
         }
     }
 
+    const onUploadProperty = (propertyType: string) => {
+
+    }
+
     return (
         <View style={style.mypropContainer}>
             <Text>welcome to properties </Text>
-            <TouchableOpacity style={style.touchOppa} onPress={ onOpenGallery }></TouchableOpacity>
+            <TouchableOpacity style={style.touchOppa} onPress={ onOpenGallery }>
+                <Image source={require("../../../public/images/add.png")} />
+            </TouchableOpacity>
             {
                 openModal?
                 <Modal animationType={"slide"} visible transparent>
-                    <ScrollView showsVerticalScrollIndicator>
-                    <View style={ style.modalContainer }>
-                        <Card style={{padding: 10}}>
-                            <Text style={{textAlign: "center", textTransform: "capitalize", fontSize: 20, fontWeight: "500"}}>upload your property!</Text>
-                        </Card>
-                        <View style={{marginTop: 10}}>
-                            <Text>Firstname11</Text>
-                            <TextInput />
-                            <Text>Firstname</Text>
-                            <TextInput />
-                            <Text>Firstname</Text>
-                            <TextInput />
-                            <Text>Firstname</Text>
-                            <TextInput />
-                            <Text>Firstname</Text>
-                            <TextInput />
-                            <Text>Firstname</Text>
-                            <TextInput />
-                            <Text>Firstname</Text>
-                            <TextInput />
-                            <Text>Firstname22</Text>
-                            <TextInput />
-                            <View style={{padding: 10, flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
-                                <TouchableOpacity>
-                                    <Image source={require("../../../public/images/def.png")} style={{width: 50, height: 50}} />
-                                </TouchableOpacity>
-                                <Text style={{paddingLeft: 10}}>uploaded image(0)</Text>
-                            </View>
-                            <View style={{flexDirection: "row", marginTop: 10}}>
-                                <View style={{flex: 1}}>
-                                    <Button title='upload' />
+                    <FlatList
+                        data={[1]}
+                        renderItem={({ item }) =>
+                        <View style={ style.modalContainer }>
+                            <Card style={{padding: 10}}>
+                                <Text style={{textAlign: "center", textTransform: "capitalize", fontSize: 20, fontWeight: "500"}}>upload your property!</Text>
+                            </Card>
+                            <View style={{marginTop: 10}}>
+                                <DropDownPicker
+                                    open = { onOpenPropToUpload }
+                                    value = { propToUploadValue }
+                                    items={ propToUploadItems }
+                                    setOpen={ setOnOpenPropToUpload }
+                                    setValue={ setPropToUploadValue }
+                                    setItems={ setPropToUploadItems }
+                                    placeholder='Select a property'
+                                    style = {{ marginTop: 5 }}
+                                />
+                                {
+                                    propToUploadValue?
+                                    propToUploadValue == "Vehicle"?
+                                    <VehicleProperty onUploadProperty={ onUploadProperty } />
+                                    :""
+                                    :
+                                    <VehicleProperty onUploadProperty={ onUploadProperty } />
+                                }
+                                <View style={{padding: 10, flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
+                                    <TouchableOpacity>
+                                        <Image source={require("../../../public/images/def.png")} style={{width: 50, height: 50}} />
+                                    </TouchableOpacity>
+                                    <Text style={{paddingLeft: 10}}>uploaded image(0)</Text>
                                 </View>
-                                <Text style={{padding: 5}}></Text>
-                                <View style={{flex: 1}}>
-                                    <Button title='cancel' onPress={() => setOpenModal(false)} />
+                                <View style={{flexDirection: "row", marginTop: 10}}>
+                                    <View style={{flex: 1}}>
+                                        <Button title='upload' />
+                                    </View>
+                                    <Text style={{padding: 5}}></Text>
+                                    <View style={{flex: 1}}>
+                                        <Button title='cancel' onPress={() => setOpenModal(false)} />
+                                    </View>
                                 </View>
                             </View>
                         </View>
-                    </View>
-                    </ScrollView>
+                        }
+                    /> 
+                    {/* end of Flatlist */}
                 </Modal>
                 :""
             }
@@ -85,7 +116,7 @@ const style = StyleSheet.create({
         padding: 10
     },
     touchOppa: {
-        backgroundColor: "#000",
+        backgroundColor: "#fff",
         width: 60,
         height: 60,
         borderRadius: 100,
@@ -94,9 +125,9 @@ const style = StyleSheet.create({
         right: 5,
     },
     modalContainer: {
-        height: Dimensions.get("window").height,
+        height: Dimensions.get("window").height+300, // just a working fix right now
         backgroundColor: "#ddd",
-        padding: 10
+        padding: 10,
     }
 });
 
