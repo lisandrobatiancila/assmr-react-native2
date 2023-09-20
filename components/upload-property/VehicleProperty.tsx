51f189654110b4398bf5867753bbf099ee66512f
san <lisandro.batiancila@gmail.com>
2023-09-20 16:47:42 +0800

    [feature][san] uploading vehicle properties

---
+++ components/upload-property/VehicleProperty.tsx
@@ -0,0 +1,89 @@
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Button, Image } from 'react-native';
+import { useState } from 'react';
+import { ImageModel } from '../../models/image/Image';
+import DocumentPicker from 'react-native-document-picker';
+
+type VehiclePropertyProps = {
+    closeModal: () => void
+};
+
+const VehicleProperty = ({ closeModal }: VehiclePropertyProps) => {
+    const [brand, setBrand] = useState<string>('');
+    const [model, setModel] = useState<string>('');
+    const [owner, setOwner] = useState<string>('');
+    const [downpayment, setDownpayment] = useState<string>('');
+    const [location, setLocation] = useState<string>('');
+    const [installmentpaid, setInstallmentpaid] = useState<string>('');
+    const [installmentduration, setInstallmentduration] = useState<string>('');
+    const [delinquent, setDelinquent] = useState<string>('');
+
+    const [fileInfo, setFileInfo] = useState<ImageModel | null>(null)
+
+    const onOpenGallery = async () => {
+        try{
+            const files = await DocumentPicker.pick({ type: DocumentPicker.types.images });
+            const file: any = files[0];
+            console.log(file);
+            
+            const form = new FormData();
+            form.append("images", file);
+
+            setFileInfo(file)
+        }
+        catch(err) {
+            console.log(err);
+        }
+    }
+    return (
+        <View>
+            <Text style = { style.textLabel }>brand</Text>
+            <TextInput value={ brand } onChangeText={ setBrand } style={ style.textInput } />
+            <Text style = { style.textLabel }>model</Text>
+            <TextInput value={ model } onChangeText={ setModel } style={ style.textInput } />
+            <Text style = { style.textLabel }>owner</Text>
+            <TextInput value={ owner } onChangeText={ setOwner } style={ style.textInput } />
+            <Text style = { style.textLabel }>downpayment</Text>
+            <TextInput value= { downpayment } onChangeText={ setDownpayment } style={ style.textInput } />
+            <Text style = { style.textLabel }>location</Text>
+            <TextInput value={ location } onChangeText={ setLocation } style={ style.textInput } />
+            <Text style = { style.textLabel }>installmentpaid</Text>
+            <TextInput value={ installmentpaid } onChangeText={ setInstallmentpaid } style={ style.textInput } />
+            <Text style = { style.textLabel }>installmentduration</Text>
+            <TextInput value={ installmentduration } onChangeText={ setInstallmentduration } style={ style.textInput } />
+            <Text style = { style.textLabel }>delinquent</Text>
+            <TextInput value={ delinquent } onChangeText={ setDelinquent } style={ style.textInput } />
+            <View style={{padding: 10, flexDirection: "row", justifyContent: "center", alignItems: "center", borderColor: fileInfo?"green":"#000", borderWidth: 2, marginTop: 10}}>
+                <TouchableOpacity onPress={ onOpenGallery }>
+                    <Image source={require("../../public/images/def.png")} style={{width: 50, height: 50}} />
+                </TouchableOpacity>
+                <Text style={{paddingLeft: 10, fontWeight: "500", textTransform: "capitalize", fontSize: 18}}>uploaded image(0)</Text>
+            </View>
+            <View style={{flexDirection: "row", marginTop: 10}}>
+                <View style={{flex: 1}}>
+                    <Button title='upload' />
+                </View>
+                <Text style={{padding: 5}}></Text>
+                <View style={{flex: 1}}>
+                    <Button title='cancel' onPress={() =>{
+                        setFileInfo(null)
+                        closeModal();
+                    }} />
+                </View>
+            </View>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    textInput: {
+        backgroundColor: "#fff",
+        borderRadius: 5
+    },
+    textLabel: {
+        textTransform: "capitalize",
+        paddingTop: 5,
+        paddingBottom: 5
+    }
+})
+
+export default VehicleProperty;
--- screens/dashboard/my-property/MyProperty.tsx
+++ screens/dashboard/my-property/MyProperty.tsx
@@ -1,14 +1,13 @@
 import { View, Text, Button, TouchableOpacity, ScrollView, FlatList, Image, Dimensions, Modal, StyleSheet } from 'react-native';
 import { useUserContext } from '../../../context/User/UserContext';
-import DocumentPicker from 'react-native-document-picker';
 import { useState } from 'react';
-import AssmrModal from '../../../components/modal/Modal';
 import { Card, TextInput } from 'react-native-paper';
 
 const MyPropertiesScreen = () => {
     const userContext = useUserContext();
     const [openModal, setOpenModal] = useState<boolean>(false);
 
+<<<<<<< Updated upstream
     const onOpenGallery = async () => {
         try{
             // const files = await DocumentPicker.pick({ type: DocumentPicker.types.images });
@@ -21,11 +20,37 @@ const MyPropertiesScreen = () => {
         catch(err) {
             console.log(err);
         }
+=======
+    const [onOpenPropToUpload, setOnOpenPropToUpload] = useState<boolean>(false);
+    const [propToUploadValue, setPropToUploadValue] = useState(null);
+    const [propToUploadItems, setPropToUploadItems] = useState<{label: string, value: string}[]>([
+        {
+            label: "Vehicle",
+            value: "Vehicle"
+        },
+        {
+            label: "Jewelry",
+            value: "Jewelry"
+        },
+        {
+            label: "Realestate",
+            value: "Realestate"
+        }
+    ]);
+    
+    const onUploadProperty = (propertyType: string) => {
+
+    }
+
+    const closeModal = () => {
+        setOpenModal(false);
+>>>>>>> Stashed changes
     }
 
     return (
         <View style={style.mypropContainer}>
             <Text>welcome to properties </Text>
+<<<<<<< Updated upstream
             <TouchableOpacity style={style.touchOppa} onPress={ onOpenGallery }></TouchableOpacity>
             {
                 openModal?
@@ -66,6 +91,40 @@ const MyPropertiesScreen = () => {
                                 <View style={{flex: 1}}>
                                     <Button title='cancel' onPress={() => setOpenModal(false)} />
                                 </View>
+=======
+            <TouchableOpacity style={style.touchOppa} onPress={ () => setOpenModal(true) }>
+                <Image source={require("../../../public/images/add.png")} />
+            </TouchableOpacity>
+            {
+                openModal?
+                <Modal animationType={"slide"} visible transparent>
+                    <FlatList
+                        data={[1]}
+                        renderItem={({ item }) =>
+                        <View style={ style.modalContainer }>
+                            <Card style={{padding: 10}}>
+                                <Text style={{textAlign: "center", textTransform: "capitalize", fontSize: 20, fontWeight: "500"}}>upload your property!</Text>
+                            </Card>
+                            <View style={{marginTop: 10}}>
+                                <DropDownPicker
+                                    open = { onOpenPropToUpload }
+                                    value = { propToUploadValue }
+                                    items={ propToUploadItems }
+                                    setOpen={ setOnOpenPropToUpload }
+                                    setValue={ setPropToUploadValue }
+                                    setItems={ setPropToUploadItems }
+                                    placeholder='Select a property'
+                                    style = {{ marginTop: 5 }}
+                                />
+                                {
+                                    propToUploadValue?
+                                    propToUploadValue == "Vehicle"?
+                                    <VehicleProperty closeModal = { closeModal } />
+                                    :""
+                                    :
+                                    <VehicleProperty closeModal = { closeModal } />
+                                }
+>>>>>>> Stashed changes
                             </View>
                         </View>
                     </View>
